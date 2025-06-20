14541a01a2b2a7579d6da99e8fa35f99733e27a7
mutatrum <mutatrum@gmail.com>
2025-06-20 16:36:30 +0200

    Whitespace

--- components/asic/include/pll.h
+++ components/asic/include/pll.h
@@ -8,4 +8,4 @@ void pll_get_parameters(float target_freq, uint16_t fb_divider_min, uint16_t fb_
                         uint8_t *fb_divider, uint8_t *refdiv, uint8_t *postdiv1, uint8_t *postdiv2,
                         float *actual_freq);
 
-#endif /* PLL_H_ */        
+#endif /* PLL_H_ */
