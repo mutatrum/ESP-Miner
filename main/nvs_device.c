fa4eaef25965f484567a6345ca41a9b3f92b1fbf
mutatrum <mutatrum@gmail.com>
2025-09-16 21:51:04 +0200

    Print NVS stats at startup

--- main/nvs_device.c
+++ main/nvs_device.c
@@ -10,11 +10,26 @@
 #include "connect.h"
 #include "global_state.h"
 
-esp_err_t NVSDevice_init(void) {
+static const char *TAG = "nvs_device";
+
+esp_err_t NVSDevice_init(void)
+{
     esp_err_t err = nvs_flash_init();
     if (err == ESP_ERR_NVS_NO_FREE_PAGES) {
         ESP_ERROR_CHECK(nvs_flash_erase());
         err = nvs_flash_init();
     }
+    
+    nvs_stats_t stats;
+    err = nvs_get_stats(NULL, &stats);
+    if (err == ESP_OK) {
+        ESP_LOGI(TAG, "Used entries: %lu", stats.used_entries);
+        ESP_LOGI(TAG, "Free entries: %lu", stats.free_entries);
+        ESP_LOGI(TAG, "Available entries: %lu", stats.available_entries);
+        ESP_LOGI(TAG, "Total entries: %lu", stats.total_entries);
+    } else {
+        ESP_LOGE(TAG, "Error getting NVS stats: %s\n", esp_err_to_name(err));
+    }
+
     return err;
 }
