31a3d086452ee0c77246ae543970beff273fea9f
adam west <adammwest1@gmail.com>
2026-02-21 18:36:52 +0100

    remove type cast

--- components/asic/include/asic_common.h
+++ components/asic/include/asic_common.h
@@ -5,6 +5,9 @@
 #include <stdbool.h>
 #include "esp_err.h"
 
+static const double NONCE_SPACE = 4294967296.0; //  2^32
+static const double ASIC_SET_NONCE_SPACE_PERCENT = 1.0;
+
 typedef enum
 {
     REGISTER_INVALID = 0,
@@ -33,9 +36,6 @@ typedef struct
     // ---- timestamp
     uint64_t timestamp_us;
 } task_result;
-
-static const double NONCE_SPACE = 4294967296.0; //  2^32
-static const double ASIC_SET_NONCE_SPACE_PERCENT = 1.0; // used by BM1362+
 
 unsigned char _reverse_bits(unsigned char num);
 int _largest_power_of_two(int num);
