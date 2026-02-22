dbcad8a00ddf622f05e935cdf196cc0d9b6051d6
adam west <adammwest1@gmail.com>
2026-02-22 16:42:23 +0100

    add some more test cases

--- components/asic/test/test_timeout.c
+++ components/asic/test/test_timeout.c
@@ -2,7 +2,7 @@
 
 #include "asic_common.h"
 
-TEST_CASE("Check asic timeout BM1397", "[common]")
+TEST_CASE("Check asic timeout 1x BM1397", "[common]")
 {
     float frequency = 450.0; // MHz
     uint16_t asic_count = 1;
@@ -18,7 +18,7 @@ TEST_CASE("Check asic timeout BM1397", "[common]")
     TEST_ASSERT_FLOAT_WITHIN(0.01, expected_ms, timeout_ms);
 }
 
-TEST_CASE("Check asic timeout BM1370", "[common]")
+TEST_CASE("Check asic timeout 2x BM1370", "[common]")
 {
     float frequency = 450.0; // MHz
     uint16_t asic_count = 2;
@@ -34,7 +34,7 @@ TEST_CASE("Check asic timeout BM1370", "[common]")
     TEST_ASSERT_FLOAT_WITHIN(0.01, expected_ms, timeout_ms);
 }
 
-TEST_CASE("Check default asic timeout BM1370", "[common]")
+TEST_CASE("Check default asic timeout 0x BM1370", "[common]")
 {
     float frequency = 450.0; // MHz
     uint16_t asic_count = 0; // 0 chip example
@@ -47,4 +47,36 @@ TEST_CASE("Check default asic timeout BM1370", "[common]")
     double timeout_ms = calculate_bm_timeout_ms(frequency, asic_count, small_cores, cores, version_size, timeout_percent, default_timeout_ms);
 
     TEST_ASSERT_FLOAT_WITHIN(0.01, default_timeout_ms, timeout_ms);
+}
+
+TEST_CASE("Check asic timeout 3x BM1370", "[common]")
+{
+    float frequency = 450.0; // MHz
+    uint16_t asic_count = 3;
+    uint16_t small_cores = 2040;
+    uint16_t cores = 128;
+    uint16_t version_size = 0xFF;
+    float timeout_percent = 0.5;
+    float default_timeout_ms = 500;
+
+    double timeout_ms = calculate_bm_timeout_ms(frequency, asic_count, small_cores, cores, version_size, timeout_percent, default_timeout_ms);
+    double expected_ms = timeout_percent * (version_size / 16.0) * (1 << 25) / (frequency * 1000) / (asic_count+1);
+
+    TEST_ASSERT_FLOAT_WITHIN(0.01, expected_ms, timeout_ms);
+}
+
+TEST_CASE("Check max asic timeout 1x BM1370", "[common]")
+{
+    float frequency = 450.0; // MHz
+    uint16_t asic_count = 1;
+    uint16_t small_cores = 2040;
+    uint16_t cores = 128;
+    uint16_t version_size = 0xFFFF;
+    float timeout_percent = 1.0;
+    float default_timeout_ms = 500;
+
+    double timeout_ms = calculate_bm_timeout_ms(frequency, asic_count, small_cores, cores, version_size, timeout_percent, default_timeout_ms);
+    double expected_ms = 4096.0 * (1 << 25) / (frequency * 1000) / (asic_count);
+
+    TEST_ASSERT_FLOAT_WITHIN(0.01, expected_ms, timeout_ms);
 }
