e36dc5376106e2db7559e2829a2ff9f9d5e05184
WantClue <wantclue@users.noreply.github.com>
2026-03-21 13:27:55 +0100

    add dropdown menu

--- main/http_server/axe-os/src/app/prime-ng.module.ts
+++ main/http_server/axe-os/src/app/prime-ng.module.ts
@@ -11,6 +11,7 @@ import { InputTextModule } from 'primeng/inputtext';
 import { SidebarModule } from 'primeng/sidebar';
 import { SliderModule } from 'primeng/slider';
 import { InputTextareaModule } from 'primeng/inputtextarea';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
 
 const primeNgModules = [
     SidebarModule,
@@ -25,6 +26,7 @@ const primeNgModules = [
     InputGroupAddonModule,
     RadioButtonModule,
     InputTextareaModule,
+    OverlayPanelModule,
 ];
 
 @NgModule({
